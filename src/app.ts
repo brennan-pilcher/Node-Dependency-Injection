de5ce4208fa720cbe567fb47a40f25e8f2ea728a
Brennan Pilcher <brennan.pilcher@gmail.com>
2021-03-11 12:13:23 -0500

    Add express and a basic route

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import "reflect-metadata";
+import * as express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+})
